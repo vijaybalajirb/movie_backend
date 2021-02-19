748a179e70f94e4b00f774c445019e8b10bf60ac
VIJAY R B <vijaybalajivj@gmail.com>
2021-02-19 18:57:01 +0530

    Add files via upload

---
+++ nodetask/server.js
@@ -0,0 +1,78 @@
+const express  =require('express')
+const bodyparser = require('body-parser')
+const cors = require('cors')
+const app = express();
+app.use(bodyparser.json())
+app.use(cors())
+
+
+movie = [
+    {
+      "title" : "Fight Club",
+      "writer" : "Chuck Palahniuk",
+      "year" : 1999,
+      "actors" : [
+      "Brad Pitt",
+      "Edward Norton"
+  ]
+  
+    },
+    {
+      "title" : "Pulp Fiction",
+      "writer" : "Quentin Tarantino",
+      "year" : 1994,
+      "actors" : [
+      "John Travolta",
+      "Uma Thurman"
+  ]
+  
+    },
+    {
+      "title" : "Inglourious Basterds",
+      "writer" : "Quentin Tarantino",
+      "year" : 2009,
+      "actors" : [
+      "Brad Pitt",
+      "Diane Kruger",
+      "Eli Roth"
+  ]
+  
+    },
+    {
+      "title" : "The Hobbit: An Unexpected Journey",
+      "writer" : "J.R.R. Tolkein",
+      "year" : 2012,
+      "franchise " : "The Hobbit"
+    },
+    {
+      "title" : "The Hobbit: The Desolation of Smaug",
+      "writer" : "J.R.R. Tolkein",
+      "year" : 2013,
+      "franchise " : "The Hobbit"
+    },
+    {
+      "title" : "The Hobbit: The Battle of the Five Armies",
+      "writer" : "J.R.R. Tolkein",
+      "year" : 2012,
+      "franchise " : "The Hobbit",
+      "synopsis":"Bilbo and Company are forced to engage in a war against an array of combatants and keep the Lonely Mountain from falling into the hands of a rising darkness"
+    },
+    {
+      "title":"Pee Wee Herman's Big Adventure"
+    },
+    {
+      "title":"Avatar"
+    }
+    
+  ]
+
+  app.get('/',(req,res)=>{
+      res.send(movie)
+
+  })
+
+
+
+
+
+app.listen(3000)
